1c7fe9796e8290e174ddf23375ed750806fbdf8a
40109771 <cyoung25@qub.ac.uk>
2016-11-10 21:15:41 +0000

    Expand graphing to show transactions with their inputs and outputs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -18,28 +18,36 @@ public class Main {
 
         // Use specific .dat file for now
         List<File> blockFiles = new ArrayList<File>();
-        File block590 = new File("C:\\Users\\Callum\\Desktop\\FinalYearProject\\Blockchain\\blk00590.dat");
+        File block590 = new File("C:\\Users\\Callum\\Desktop\\FinalYearProject\\Blockchain\\blk00277.dat");
         blockFiles.add(block590);
 
         BlockFileLoader blockFileLoader = new BlockFileLoader(params, blockFiles);
         // Only use first block for now as it's easier to set up the program using a smaller number of transactions
         Block block = blockFileLoader.next();
 
-        // Create node in Neo4J for each input address
         Driver driver = GraphDatabase.driver("bolt://localhost", AuthTokens.basic("neo4j", "password"));
         Session session = driver.session();
-        for (Transaction transaction : block.getTransactions()) {
-            System.out.println("Transaction hash: " + transaction.getHashAsString());
+
+        String inputName, outputName;
+        int inputNumber = 1;
+        int outputNumber = 1;
+        for (Transaction transaction : block.getTransactions().subList(0,30)) {
+            String transHash = transaction.getHashAsString();
+             session.run("CREATE(t:Transaction {hash:'" + transHash + "'})");
+
             for (TransactionInput input : transaction.getInputs()) {
-                System.out.println("Transaction input addresses: ");
-                try {
-                    System.out.println("- " + input.getFromAddress());
-                    session.run( "CREATE (a:TransactionInputAddress {address:'" + input.getFromAddress() + "'})" );
-                } catch (ScriptException e) {
-                    System.out.println(e);
-                }
+                inputName = "input" + inputNumber;
+                session.run("CREATE(i:Input {name:'" + inputName + "'})");
+                session.run("MATCH(i:Input {name:'" + inputName + "'}),(t:Transaction {hash:'" + transHash + "'}) CREATE(i)-[:INPUT]->(t)");
+                inputNumber++;
             }
-            System.out.println();
+
+            for (TransactionOutput output : transaction.getOutputs()) {
+                outputName = "output" + outputNumber;
+                session.run("CREATE(o:Output {name:'" + outputName + "'})");
+                session.run("MATCH(t:Transaction {hash:'" + transHash + "'}),(o:Output {name:'" + outputName + "'}) CREATE(t)-[:OUTPUT]->(o)");
+                outputNumber++;
+            }
         }
         session.close();
         driver.close();
